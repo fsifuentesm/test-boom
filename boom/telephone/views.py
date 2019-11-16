1a459cd0644c5df80cbbc92b06703ddec76c0f55
fsifuentesm <fsifuentesm@hotmail.com>
2019-11-15 19:23:44 -0600

    reading and print csv view

--- boom/telephone/views.py
+++ boom/telephone/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
-from django.core.mail import EmailMessage
+import csv
 
 # Create your views here.
 
 def form(request):
     print("hola")
+    with open('numbers.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            print(row['numbers'])
+
     return render(request, 'home.html', locals())
